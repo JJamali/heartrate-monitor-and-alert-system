3d40cfbec89c357e1ef5f6b402a245aae98f1e8e
Jordan Jamali <31671594+JJamali@users.noreply.github.com>
2022-07-15 19:39:36 -0400

    Update mainode.py

--- mainode.py
+++ mainode.py
@@ -1,6 +1,12 @@
 import serial
 import tkinter as tk
 
+# set up fonts for pop up window stuff
+LARGE_FONT= ("Verdana", 12)
+NORM_FONT = ("Helvetica", 80)
+SMALL_FONT = ("Helvetica", 8)
+
+# takes in string msg, returns pop up window when called with msg
 def popupmsg(msg):
     popup = tk.Tk()
     popup.wm_title("!")
@@ -10,16 +16,10 @@ def popupmsg(msg):
     B1.pack()
     popup.mainloop()
 
+# connect python to arduino serial readings
 ser = serial.Serial()
 ser.baudrate = 9600
-ser.port = 'PORTNAME'
-
-
-
-LARGE_FONT= ("Verdana", 12)
-NORM_FONT = ("Helvetica", 80)
-SMALL_FONT = ("Helvetica", 8)
-
+ser.port = 'PORTNAME' # note: this changes with different environments
 
 
 # list of the previous 30 beat times
@@ -31,15 +31,18 @@ beat_intervals = []
 # list of heartrates 
 heartrates = []
 
-# detect R wave of heartbeat
+# run indefinitely to constantly detect heartbeat
+while true:
+    # detect R wave of heartbeat
+    if R detected:
+        # remove first item from beat times and append current time
+        beat_times.append(datetime.now())
+        beat_times.pop(0)
 
-if R:
-    # remove first item from beat times and append current time
-    beat_times.append(datetime.now())
-    beat_times.pop(0)
+        # add heartrate over last 30 beats to heartrates list
+        heartrates.append((beat_times[len(beat_times)] - beat_times[0]) / 30) 
     
-    # add heartrate over last 30 beats
-    heartrates.append((beat_times[len(beat_times)] - beat_times[0]) / 30) 
+
     
 
 
