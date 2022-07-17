fc349c9f1555420fa52aee681501203744efad2f
Jordan Jamali <31671594+JJamali@users.noreply.github.com>
2022-07-17 15:15:07 -0400

    Update core.py

--- python code/core.py
+++ python code/core.py
@@ -1,5 +1,6 @@
 import serial
 import tkinter as tk
+import statistics
 
 # set up fonts for pop up window stuff
 LARGE_FONT= ("Verdana", 12)
@@ -31,17 +32,57 @@ beat_intervals = []
 # list of heartrates 
 heartrates = []
 
+r_wave = False
+arr_check = False
+
 # run indefinitely to constantly detect heartbeat
-while true:
+while True:
+
+    input = ser.read()
+    print(input)
+
+    #======R wave======
+
+    # check if in R wave
+    if {{input > val}}:
+        r_wave = True
+    else:
+        r_wave = False
+
     # detect R wave of heartbeat
-    if R detected:
+    if r_wave:
         # remove first item from beat times and append current time
         beat_times.append(datetime.now())
-        beat_times.pop(0)
+        if len(beat_times) > 2:
+            beat_times.pop(0)
+
+        lastHR = 60000 / (beat_times[1] - beat_times[0])
 
         # add heartrate over last 30 beats to heartrates list
-        heartrates.append((beat_times[len(beat_times)] - beat_times[0]) / 30) 
-    
+        heartrates.append(lastHR)
+        if len(heartrates) > 29:
+            heartrates.pop(0)
+
+        # enter alarm logic only if we have collected enough heartrates
+        if len(heartrates) > 30:
+            if lastHR < 50:
+                if statistics.mean(heartrates) > 40:
+                    #mild alert
+                else:
+                    #high alert
+
+            elif lastHR > 120:
+                Enter high HR detection algorithm
+
+
+            # ======arrhythmia======
+            # if difference between last two beats exceeds 30%
+            if math.abs((lastHR - heartrates[len(heartrates) - 2])/lastHR) > 0.3:
+                arr_check = True
+
+
+            # wait 15ms until R is over
+            time.sleep(0.015)
 
     
 
